2a54748cad627e5582353fbd6ea92b4613092532
Nafiz Anam <nafiz.anam002@gmail.com>
2021-10-25 13:24:09 +0600

    updated file

--- src/Firebase/Hooks/useFirebase.js
+++ src/Firebase/Hooks/useFirebase.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import initializeAuth from "../Auth/firebase.init";
 import {
     getAuth,
     signInWithPopup,
     GoogleAuthProvider,
     signOut,
+    onAuthStateChanged,
 } from "firebase/auth";
 
 //initializing firebase here
@@ -14,15 +15,30 @@ const useFirebase = () => {
     // states we need
     const [user, setUser] = useState({});
     const [error, setError] = useState("");
+
     // auth for firebase
     const auth = getAuth();
+
     // providers for different login methods
     const googleProvider = new GoogleAuthProvider();
+
     // login functions here
     //google login
     const loginUsingGoogle = () => {
         return signInWithPopup(auth, googleProvider);
     };
+
+    //observer function here
+    useEffect(() => {
+        const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser({});
+            }
+        });
+        return () => unsubscribed;
+    }, []);
 
     // log out function here
     const logout = () => {
@@ -37,8 +53,8 @@ const useFirebase = () => {
         logout,
         error,
         setUser,
-        setError,
         auth,
+        setError,
     };
 };
 
